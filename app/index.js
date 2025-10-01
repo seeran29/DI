3af2d376f83fb12a0cc6f85a67791ddbdc1ccae8
Manel Viel <mviel@florida-uni.es>
2025-10-01 15:38:11 +0200

    projecte base

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+const  Index = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hola Manel!!</Text>
+      <Text>Primera pàgina de prova!!!.</Text>
+    </View>
+  );
+}
+
+export default Index;
